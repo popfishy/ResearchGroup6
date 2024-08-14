d86c26a9c9e792e76294f001a38227d960b3ec07
popfishy <1301350150@qq.com>
2024-08-14 15:39:29 +0800

    删除XTDrone文件夹

--- src/XTdrone/control/XTDGroundControl/python/resize.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-im = Image.open('robocup.jpg')
-imBackground = im.resize((700, 470))  #700 470 840 564
-imBackground.save('outdoor1_resize.jpg')
--- src/XTdrone/control/multi_precision_landing.sh
+++
@@ -1,3 +0,0 @@
-python precision_landing.py iris 0&
-python precision_landing.py iris 1&
-python precision_landing.py iris 2
