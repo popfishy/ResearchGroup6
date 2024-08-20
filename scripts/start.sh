896b5ac98095f1e5b0e09c29db9345324aac0a79
popfishy <1301350150@qq.com>
2024-08-20 09:38:09 +0800

    TODO：提交README文档

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,15 +15,22 @@ echo 'dji' | sudo -S chmod 777 QGroundControl.AppImage
 # 定义变量
 UAV_NUM=5
 
+# 启动仿真环境
 gnome-terminal -- bash -c "cd ~/PX4_Firmware && roslaunch px4 multi_vehicle.launch"
 sleep 10
+# 启动QGC地面站
 gnome-terminal -- bash -c "cd ~/ResearchGroup6/scripts && ./QGroundControl.AppImage"
 sleep 2
+# 启动无人机通信
 gnome-terminal -- bash -c "cd ~/ResearchGroup6/src/fixed_wing_formation/scripts && bash multi_vehicle_communication.sh; exec bash"
 sleep 1.5
+# 启动无人机键盘控制，需手动解锁起飞，按o发送航点后切换至offboard模式。在未接收到公司数据前，无人机将在(0,0,200)处盘旋
 gnome-terminal -- bash -c "cd ~/ResearchGroup6/src/fixed_wing_formation/scripts && python plane_keyboard_control.py $UAV_NUM "
 sleep 1
+# 发送无人机数据到公司
 gnome-terminal -- bash -c "cd ~/ResearchGroup6/src/communication/scripts && python send_data_to_company.py plane $UAV_NUM "
 sleep 1
+# 等待接收公司数据
 gnome-terminal -- bash -c "cd ~/ResearchGroup6/src/communication/scripts && python receive_company_data.py; exec bash"
+# 对公司数据进行解析，生成无人机的航迹，并进行仿真
 gnome-terminal -- bash -c "cd ~/ResearchGroup6/src/fixed_wing_formation/scripts && python GVF_ode_node.py; exec bash"
